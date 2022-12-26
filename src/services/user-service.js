4ab3f99c12fe3578e7a4380cd0a557d5027dd9cc
arungupta7221 <48250441+arungupta7221@users.noreply.github.com>
2022-12-26 19:36:45 +0530

    added token gen logic from signin

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,6 +1,7 @@
 const UserRepository = require('../repository/user-repository')
 const jwt = require('jsonwebtoken')
 const { JWT_KEY } = require('../config/configServer')
+const bcrypt = require('bcrypt')
 class UserService {
   constructor() {
     this.userRepository = new UserRepository()
@@ -12,6 +13,25 @@ class UserService {
       return user
     } catch (error) {
       console.log('Something went wrong in the service layer')
+      throw error
+    }
+  }
+  async signIn(email, plainPassword) {
+    try {
+      // step 1-> fetch the user using the email
+      const user = await this.userRepository.getUserByEmail(email)
+      // step 2-> compare incoming plain password with stores encrypted password
+      const passwordsMatch = this.checkPassword(plainPassword, user.password)
+
+      if (!passwordsMatch) {
+        console.log("Password doesn't match")
+        throw { error: 'Incorrect password' }
+      }
+      // step 3-> if passwords match then create a token and send it to the user
+      const newJWT = this.createToken({ email: user.email, id: user.id })
+      return newJWT
+    } catch (error) {
+      console.log('Something went wrong in the signIn process')
       throw error
     }
   }
@@ -32,6 +52,15 @@ class UserService {
       return response
     } catch (error) {
       console.log('Something went wrong in the token creation', error)
+      throw error
+    }
+  }
+
+  checkPassword(userInputPlainPassword, encryptedPassword) {
+    try {
+      return bcrypt.compareSync(userInputPlainPassword, encryptedPassword)
+    } catch (error) {
+      console.log('Something went wrong in the comparing password')
       throw error
     }
   }
