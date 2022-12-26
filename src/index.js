871cabbd0c4665452abbdcafc03d735447ee2e28
arungupta7221 <48250441+arungupta7221@users.noreply.github.com>
2022-12-26 17:20:01 +0530

    Added JWT sign and verify logic

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const apiRoutes = require('./routes/index')
 
 // const {User} = require('./models/index');
 // const bcrypt = require('bcrypt');
-
+// const UserService = require('./services/user-service')
 const app = express()
 
 const prepareAndStartServer = () => {
@@ -21,6 +21,15 @@ const prepareAndStartServer = () => {
     // const user = await User.findByPk(3);
     // const response = bcrypt.compareSync(incomingpassword, user.password);
     // console.log(response);
+
+    // const service = new UserService()
+    // const newToken = service.createToken({ email: 'arun@gmail.com', id: 1 })
+    // console.log('new token is ', newToken)
+
+    // const token =
+    //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFydW5AZ21haWwuY29tIiwiaWQiOjEsImlhdCI6MTY3MjA1NDgxNSwiZXhwIjoxNjcyMDU4NDE1fQ.HQSUV6XKhcGNgt-rUoVcc2C4VkgREof5Gw8g0fJeJ1c'
+    // const response = service.verifyToken(token)
+    // console.log(response)
   })
 }
 prepareAndStartServer()
